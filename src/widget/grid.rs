ed1897bf94b6027f1b9940e9340e854d6a61e0dd
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2024-08-25 12:48:31 -0400

    Restructuring and refactors

--- src/widget/grid.rs
+++ src/widget/grid.rs
@@ -1,13 +1,26 @@
 use iced::{
     widget::{button, column, container, row, Space},
-    Background, Color, Element, Length,
+    Background, Color, Element, Length, Task,
 };
 
 use crate::MatchupResult;
 
 #[derive(Debug, Clone)]
-pub enum GridMessage<'a> {
-    Select(&'a MatchupResult),
+pub enum GridMessage {
+    Select(usize),
+    Loaded(stats::Statistics)
+
+}
+
+pub enum Grid {
+    Loading,
+    Loaded(GridState)
+}
+
+pub struct GridState {
+    grid_cells: Vec<GridCell>,
+    matchup_results: Vec<MatchupResult>,
+    status: GridStatus,
 }
 
 #[derive(Default, Debug, Clone)]
@@ -17,19 +30,13 @@ pub enum GridStatus {
     Focused(MatchupResult),
 }
 
-#[derive(Debug, Clone)]
-pub struct Grid {
-    grid_cells: Vec<GridCell>,
-    status: GridStatus,
-}
-
 impl Grid {
-    pub fn new(matchup_results: Vec<MatchupResult>) -> Grid {
-        let grid_cells = matchup_results.into_iter().map(GridCell::new).collect();
-        Grid {
-            grid_cells,
-            status: GridStatus::Idle,
-        }
+    pub fn new(matchup_results: Vec<MatchupResult>) -> (Grid, Task<GridMessage>) {
+        (Grid::Loading, Task::perform(Grid::recalculate_statistics(matchup_results), GridMessage::Loaded))
+    }
+
+    async fn recalculate_statistics(matchup_results: Vec<MatchupResult>) -> Statistics {
+
     }
 
     pub fn update(&mut self, _message: GridMessage) {}
@@ -40,7 +47,8 @@ impl Grid {
         let rows = self.grid_cells.chunks(n).map(|r| {
             row(r
                 .iter()
-                .map(|e| e.view().map(|_| GridMessage::Select(&e.matchup_result))))
+                .enumerate()
+                .map(|e| e.view().map(|(x, _)| GridMessage::Select())))
             .into()
         });
 
@@ -50,8 +58,8 @@ impl Grid {
 
 #[derive(Debug, Clone)]
 struct GridCell {
+    id: usize,
     is_selected: bool,
-    matchup_result: MatchupResult,
 }
 
 #[derive(Debug, Clone)]
@@ -61,9 +69,9 @@ enum CellMessage {
 }
 
 impl GridCell {
-    pub fn new(matchup_result: MatchupResult) -> GridCell {
+    pub fn new(id: usize) -> GridCell {
         GridCell {
-            matchup_result,
+            id,
             is_selected: false,
         }
     }
@@ -82,13 +90,34 @@ impl GridCell {
                 .style(|_, status| {
                     let bg_color = match status {
                         button::Status::Hovered => Color::from_rgb8(128, 128, 128),
-                        _ => Color::BLACK
+                        _ => Color::BLACK,
                     };
 
                     button::Style::default().with_background(Background::Color(bg_color))
                 })
                 .width(20)
-                .height(20)
-        ).padding(0).into()
+                .height(20),
+        )
+        .padding(0)
+        .into()
+    }
+}
+
+mod stats {
+    #[derive(Debug, Clone)]
+    pub struct Statistics {
+        average: f64,
+        average_values: Vec<f64>,
+        colors: Vec<Color>
+    }
+
+
+    fn blend_colors(first: Color, second: Color, a: f32) -> Color {
+        let x = 1.0 - a;
+        Color::from_rgb(
+            first.r * x + second.r * a,
+            first.g * x + second.g + a,
+            first.b * x + second.b * a,
+        )
     }
 }
