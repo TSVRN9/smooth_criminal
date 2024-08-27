5703b205f11e1be3b94b748af280eb2f31157572
TSVRN9 <66744142+TSVRN9@users.noreply.github.com>
2024-08-27 09:35:36 -0400

    Continue defining state and messages

--- src/widget/app.rs
+++ src/widget/app.rs
@@ -4,7 +4,10 @@ use iced::{widget::text, Color, Element, Task};
 use rayon::prelude::*;
 
 use crate::{
-    colors::blend_colors, run_competition, strategies::{classic, continuous, tsvrn9}, GameResult, MatchupResult, Strategy
+    colors::blend_colors,
+    run_competition,
+    strategies::{classic, continuous, tsvrn9},
+    GameResult, MatchupResult, Strategy,
 };
 
 use super::grid::Grid;
@@ -13,26 +16,46 @@ use super::grid::Grid;
 pub enum ResultsInspector {
     #[default]
     Loading,
+    Raw(RawState),
     Loaded(State),
-
 }
 
 pub struct State {
     grid: Grid,
-    strategy_names: Vec<&'static str>,
-    matchup_results: Vec<MatchupResult>,
-    stats: HashMap<&'static str, Stat>,
+    data: Data,
     selected_stat: &'static str,
-    colors: Vec<Color>,
+    colors: Colors,
     filters: Vec<StatFilter>,
 }
 
 #[derive(Debug, Clone)]
-pub struct Stat {
-    values: Vec<f64>,
-    strategy_averages: Vec<f64>
+pub struct RawState {
+    data: Data,
+    selected_stat: &'static str,
+    filters: Vec<StatFilter>,
 }
 
+#[derive(Debug, Clone)]
+pub struct Data {
+    strategy_names: Vec<&'static str>,
+    matchup_results: Vec<MatchupResult>,
+    stats: HashMap<&'static str, Stat>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Colors {
+    cell_colors: Vec<Color>,
+    strategy_colors: Vec<Color>,
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Stat {
+    values: Vec<f64>,
+    strategy_averages: Vec<f64>,
+}
+
+#[derive(Debug, Clone)]
 pub enum StatFilter {
     HideRow(usize),
     HideColumn(usize),
@@ -40,56 +63,92 @@ pub enum StatFilter {
 
 #[derive(Debug, Clone)]
 pub enum Message {
-    Loaded(Vec<&'static str>, Vec<MatchupResult>, HashMap<&'static str, Stat>),
+    Raw(Data),
+    RecalculateColor,
+    Loaded(Colors),
 }
 
 impl ResultsInspector {
     pub fn new() -> (ResultsInspector, Task<Message>) {
-        (
-            Self::Loading,
-            Task::perform(
-                load(),
-                |(strategy_names, matchup_results, statistics)| Message::Loaded(strategy_names, matchup_results, statistics),
-            ),
-        )
+        (Self::Loading, Task::perform(load(), Message::Raw))
     }
 
-    pub fn update(&mut self, message: Message) {
+    pub fn update(&mut self, message: Message) -> Task<Message> {
         match message {
-            Message::Loaded(strategy_names, matchup_results, stats) => {
-                let n = strategy_names.len();
-                *self = ResultsInspector::Raw(State {
-                    grid: Grid::new(n, n),
-                    strategy_names, matchup_results, selected_stat: *stats.iter().next().unwrap().0, stats, filters: vec![]
-                })
+            Message::Raw(data) => {
+                if let ResultsInspector::Loading = self {
+                    *self = ResultsInspector::Raw(RawState {
+                        selected_stat: data.stats.keys().next().unwrap(),
+                        data,
+                        filters: vec![],
+                    });
+
+                    self.update(Message::RecalculateColor)
+                } else {
+                    panic!("Unexpected message");
+                }
+            }
+            Message::RecalculateColor => {
+                let stat = match self {
+                    ResultsInspector::Loaded(state) => {
+                        let selected_stat = state.selected_stat;
+                        &state.data.stats[selected_stat]
+                    }
+                    ResultsInspector::Raw(raw_state) => {
+                        let selected_stat = raw_state.selected_stat;
+                        &raw_state.data.stats[selected_stat]
+                    }
+                    _ => panic!("Unexpected state"),
+                };
+
+                Task::perform(calculate_cell_and_strategy_colors(stat), Message::Loaded)
+            }
+            Message::Loaded(colors) => {
+                if let ResultsInspector::Loaded(state) = self {
+                    state.colors = colors;
+                }
+                Task::none()
             }
         }
     }
 
     pub fn view(&self) -> Element<Message> {
         match self {
-            ResultsInspector::Loading => text("Loading...").into(),
-            ResultsInspector::Raw(state) => {
+            ResultsInspector::Loading | ResultsInspector::Raw(_) => text("Loading...").into(),
+            ResultsInspector::Loaded(state) => {
                 todo!();
-            },
+            }
         }
     }
 }
 
-async fn load() -> (Vec<&'static str>, Vec<MatchupResult>, HashMap<&'static str, Stat>) {
-    let strategies: Vec<_> =
-        vec![classic::all(), continuous::all(), tsvrn9::all()]
-            .into_iter()
-            .flatten()
-            .collect();
+async fn load() -> Data {
+    let strategies: Vec<_> = vec![classic::all(), continuous::all(), tsvrn9::all()]
+        .into_iter()
+        .flatten()
+        .collect();
 
     let strategy_names = strategies.iter().map(|(name, _)| *name).collect();
     let grid_width = strategies.len();
     let matchup_results = run_competition(strategies).await;
 
     let (point_difference, points_per_round) = tokio::join!(
-        calculate_stat(|MatchupResult { overall_result: GameResult(a, b), .. }| a - b, &matchup_results, grid_width),
-        calculate_stat(|MatchupResult { overall_result: GameResult(a, _), .. }| *a, &matchup_results, grid_width)
+        calculate_stat(
+            |MatchupResult {
+                 overall_result: GameResult(a, b),
+                 ..
+             }| a - b,
+            &matchup_results,
+            grid_width
+        ),
+        calculate_stat(
+            |MatchupResult {
+                 overall_result: GameResult(a, _),
+                 ..
+             }| *a,
+            &matchup_results,
+            grid_width
+        )
     );
 
     let mut stats = HashMap::new();
@@ -97,19 +156,19 @@ async fn load() -> (Vec<&'static str>, Vec<MatchupResult>, HashMap<&'static str,
     stats.insert("Point difference", point_difference);
     stats.insert("Points per round", points_per_round);
 
-    (strategy_names, matchup_results, stats)
+    Data {
+        strategy_names,
+        matchup_results,
+        stats,
+    }
 }
 
 async fn calculate_stat(
     by: fn(&MatchupResult) -> f64,
     results: &Vec<MatchupResult>,
     grid_width: usize,
-) -> Stat
-{
-    let values: Vec<_> = results
-        .iter()
-        .map(by)
-        .collect();
+) -> Stat {
+    let values: Vec<_> = results.iter().map(by).collect();
 
     let strategy_averages = values
         .chunks_exact(grid_width)
@@ -117,22 +176,20 @@ async fn calculate_stat(
         .collect::<Vec<_>>();
 
     Stat {
-        values, strategy_averages
+        values,
+        strategy_averages,
     }
 }
 
-async fn calculate_cell_and_strategy_colors(
-    stat: &Stat,
-) -> (Vec<Color>, Vec<Color>)
-{
+async fn calculate_cell_and_strategy_colors(stat: &Stat) -> Colors {
     let average = stat.strategy_averages.iter().sum::<f64>() / stat.values.len() as f64;
 
-    let (cell_colors, label_colors) = tokio::join!(
+    let (cell_colors, strategy_colors) = tokio::join!(
         calculate_colors(average, &stat.values, Color::BLACK),
         calculate_colors(average, &stat.strategy_averages, Color::WHITE)
     );
 
-    (cell_colors, label_colors)
+    Colors { cell_colors, strategy_colors }
 }
 
 async fn calculate_colors(standard: f64, values: &Vec<f64>, default: Color) -> Vec<Color> {
@@ -158,6 +215,10 @@ async fn calculate_colors(standard: f64, values: &Vec<f64>, default: Color) -> V
 }
 
 fn calculate_color(deviation_percent: f32, default: Color) -> Color {
-    let to_blend_with = if deviation_percent > 0.0 { crate::colors::BLUE } else { crate::colors::RED };
+    let to_blend_with = if deviation_percent > 0.0 {
+        crate::colors::BLUE
+    } else {
+        crate::colors::RED
+    };
     blend_colors(default, to_blend_with, deviation_percent.abs())
 }
